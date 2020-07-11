8e82d928a3d8c58608a327808369b645ba754662
Sebastian Pietras <seba00767@gmail.com>
2020-07-12 01:36:55 +0200

    Improved sh

--- Linux/sh/README.md
+++ Linux/sh/README.md
@@ -1,11 +1,18 @@
 # Requirements
 
+In order of quality:
+
 - ```play``` command from [sox package](http://sox.sourceforge.net)
 
 or
 
-- ```beep```
-- ```beep``` must have suid bit set (```sudo chmod 4755 /usr/bin/beep```)
+- ```pactl``` command from [Pulse Audio](http://pulseaudio.org)
+
+or
+
+- ```speaker-test``` command from [ALSA](https://www.alsa-project.org/)
+
+(In the absence of the above commands, a generic console sound will be used.)
 
 # Usage
 
--- Linux/sh/play.sh
+++ Linux/sh/play.sh
@@ -8,6 +8,21 @@ print_usage()
     echo "sheet - file with notes"
 }
 
+is_available()
+{
+    [ -x "$(command -v "$1")" ]
+}
+
+round()
+{
+    echo "$1" | awk '{print int($1+0.5)}'
+}
+
+mili_to_seconds()
+{
+    echo "$1/1000" | bc -sl
+}
+
 if [ "$#" -ne 1 ]
 then
     echo "Exactly one argument is needed"
@@ -29,28 +44,62 @@ then
     exit 3
 fi
 
-if [ -x "$(command -v play)" ]
+if is_available 'play'
 then
     makeBeep () 
     {
         for i in $(seq 1 "${4:-1}");
         do
-            play -n synth "$(echo "$2/1000" | bc -l)" sine "$1" >/dev/null 2>&1
-            sleep "$(echo "$3/1000" | bc -l)"
+            play -q -n synth "$(mili_to_seconds "$2")" sine "$1" && sleep "$(mili_to_seconds "$3")"
         done
     }
-elif [ -x "$(command -v beep)" ]
+elif is_available 'pactl'
 then
     makeBeep () 
     {
-        beep -f "$1" -l "$2" -D "$3" -r "${4:-1}"
+        for i in $(seq 1 "${4:-1}");
+        do
+            # setsid is here because pactl probably has its own trap logic that doesn't unload the module
+            # more here: https://unix.stackexchange.com/a/350056 and https://unix.stackexchange.com/a/461803
+            module="$(setsid pactl load-module module-sine frequency="$(round "$1")")" && {
+                sleep "$(mili_to_seconds "$2")"
+                setsid pactl unload-module "$module"
+                sleep "$(mili_to_seconds "$3")"
+            }
+        done
+    }
+elif is_available 'speaker-test'
+then
+    makeBeep () 
+    {
+        for i in $(seq 1 "${4:-1}");
+        do
+            # it's hard to make speaker-test work, 
+            # setting channels to 32 gave the best results, i don't know why
+            # but then i only have sound on the left side
+            timeout -s SIGHUP "$(mili_to_seconds "$2")" speaker-test -t sine -f "$1" -c 32 >/dev/null
+            sleep "$(mili_to_seconds "$3")"
+        done
     }
 else
-    echo "neither play nor beep is installed"
-    print_usage
-    exit 4
+    echo 'No usable command found. Using generic console sound'
+    makeBeep () 
+    {
+        for i in $(seq 1 "${4:-1}");
+        do
+            printf '\a'  
+            sleep "$(mili_to_seconds "$2")"
+            sleep "$(mili_to_seconds "$3")"
+        done
+    }
 fi
 
 while read -r frequency length delay repeats; do
-    makeBeep "$frequency" "$length" "$delay" "$repeats"
+    # make beep in background and wait
+    # background ignores signals (so sound will not be interrupted)
+    # but main shell exits immediately
+    (
+        trap '' 1 2 3 6 15
+        makeBeep "$frequency" "$length" "$delay" "$repeats"
+    ) & wait
 done < "$1"
